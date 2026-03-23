f7dccb741f58b0c874c0db3029e42970366c0d07
djdebonis <davidjdebonis@gmail.com>
2026-03-23 12:11:25 -0600

    start streamlit app.

---
+++ garden_plot_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title('Visual Garden Planner for Denver and Nearby Lattitudes: Flower and Foliage')
+
